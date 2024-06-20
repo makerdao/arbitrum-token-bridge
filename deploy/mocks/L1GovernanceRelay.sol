1ad3d9cb667ab7476430537e46139185eab134e1
telome <>
2024-06-20 15:06:33 +0300

    More intendation fix

--- deploy/mocks/L1GovernanceRelay.sol
+++ deploy/mocks/L1GovernanceRelay.sol
@@ -60,61 +60,61 @@ abstract contract L1CrossDomainEnabled {
         inbox = IInbox(_inbox);
     }
 
-  modifier onlyL2Counterpart(address l2Counterpart) {
-      // a message coming from the counterpart gateway was executed by the bridge
-      address bridge = inbox.bridge();
-      require(msg.sender == bridge, "NOT_FROM_BRIDGE");
+    modifier onlyL2Counterpart(address l2Counterpart) {
+        // a message coming from the counterpart gateway was executed by the bridge
+        address bridge = inbox.bridge();
+        require(msg.sender == bridge, "NOT_FROM_BRIDGE");
 
-      // and the outbox reports that the L2 address of the sender is the counterpart gateway
-      address l2ToL1Sender = IOutbox(IBridge(bridge).activeOutbox()).l2ToL1Sender();
-      require(l2ToL1Sender == l2Counterpart, "ONLY_COUNTERPART_GATEWAY");
-      _;
-  }
+        // and the outbox reports that the L2 address of the sender is the counterpart gateway
+        address l2ToL1Sender = IOutbox(IBridge(bridge).activeOutbox()).l2ToL1Sender();
+        require(l2ToL1Sender == l2Counterpart, "ONLY_COUNTERPART_GATEWAY");
+        _;
+    }
 
-  function sendTxToL2(
-      address target,
-      address user,
-      uint256 maxSubmissionCost,
-      uint256 maxGas,
-      uint256 gasPriceBid,
-      bytes memory data
-  ) internal returns (uint256) {
-      uint256 seqNum = inbox.createRetryableTicket{value: msg.value}(
-          target,
-          0, // we always assume that l2CallValue = 0
-          maxSubmissionCost,
-          user,
-          user,
-          maxGas,
-          gasPriceBid,
-          data
-      );
-      emit TxToL2(user, target, seqNum, data);
-      return seqNum;
-  }
+    function sendTxToL2(
+        address target,
+        address user,
+        uint256 maxSubmissionCost,
+        uint256 maxGas,
+        uint256 gasPriceBid,
+        bytes memory data
+    ) internal returns (uint256) {
+        uint256 seqNum = inbox.createRetryableTicket{value: msg.value}(
+            target,
+            0, // we always assume that l2CallValue = 0
+            maxSubmissionCost,
+            user,
+            user,
+            maxGas,
+            gasPriceBid,
+            data
+        );
+        emit TxToL2(user, target, seqNum, data);
+        return seqNum;
+    }
 
-  function sendTxToL2NoAliasing(
-      address target,
-      address user,
-      uint256 l1CallValue,
-      uint256 maxSubmissionCost,
-      uint256 maxGas,
-      uint256 gasPriceBid,
-      bytes memory data
-  ) internal returns (uint256) {
-      uint256 seqNum = inbox.createRetryableTicketNoRefundAliasRewrite{value: l1CallValue}(
-          target,
-          0, // we always assume that l2CallValue = 0
-          maxSubmissionCost,
-          user,
-          user,
-          maxGas,
-          gasPriceBid,
-          data
-      );
-      emit TxToL2(user, target, seqNum, data);
-      return seqNum;
-  }
+    function sendTxToL2NoAliasing(
+        address target,
+        address user,
+        uint256 l1CallValue,
+        uint256 maxSubmissionCost,
+        uint256 maxGas,
+        uint256 gasPriceBid,
+        bytes memory data
+    ) internal returns (uint256) {
+        uint256 seqNum = inbox.createRetryableTicketNoRefundAliasRewrite{value: l1CallValue}(
+            target,
+            0, // we always assume that l2CallValue = 0
+            maxSubmissionCost,
+            user,
+            user,
+            maxGas,
+            gasPriceBid,
+            data
+        );
+        emit TxToL2(user, target, seqNum, data);
+        return seqNum;
+    }
 }
 
 // Relay a message from L1 to L2GovernanceRelay
